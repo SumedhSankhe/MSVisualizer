e35c81beb92214b544d804be291847e67d8f5f6e
Sumedh R. Sankhe <sumedh.sankhe@gmail.com>
2022-08-05 14:51:15 -0700

    univariate module updates

--- R/univariatePanel.R
+++ R/univariatePanel.R
@@ -1,8 +1,9 @@
 univariatePlotPanelUI <- function(id) {
   ns <- NS(id)
   tagList(
+    h3("Univariate Plot"),
     fluidRow(
-      box( title = "Univariate Plot", width = 4, status = "warning",
+      box( title = 'Control Box', width = 4, status = "primary",
            tabBox(id = "univariatePlot", height = "100%", width = "100%",
                   tabPanel("univariatePlot",
                            selectInput(inputId = ns('sample_group'),
@@ -14,24 +15,26 @@ univariatePlotPanelUI <- function(id) {
                   ),
                   tabPanel(tagList(shiny::icon("gear"), "Settings"),
                            uiOutput(ns("colorChooser")),
-                           numericInput(inputId = ns('x_axis_text_angle'), label = 'x-axis text angle: ', value='0'),
-                           numericInput(inputId = ns('plot_width'), label = 'Plot Width: ', value='15'),
-                           numericInput(inputId = ns('plot_height'), label = 'Plot Height: ', value='10'),
+                           numericInput(inputId = ns('x_axis_text_angle'),
+                                        label = 'x-axis text angle: ', value='0'),
+                           numericInput(inputId = ns('plot_width'),
+                                        label = 'Plot Width: ', value='15'),
+                           numericInput(inputId = ns('plot_height'),
+                                        label = 'Plot Height: ', value='10'),
                            selectInput(inputId = ns('plot_type'),
                                        label = 'Plot Type: ',
                                        choices = c('svg', 'png', 'pdf')
                            )))
       ),
-      box(
-        status = "warning", width = 8,
-        tabBox(height = '100%', width = '100%',
-               (div(
-                 style='width:1000px;overflow-x: scroll;height:1000px;overflow-y: scroll;',
-                 shinycssloaders::withSpinner(
-                   plotly::plotlyOutput(ns("univariatePlot")))
-               ))
-        ),
-        downloadButton(ns('downloadPlot'), "Download Plot")
+      box(title = 'Visualization', status = "warning", width = 8,
+          tabBox(height = '100%', width = '100%',
+                 (div(
+                   style='width:auto;overflow-x: scroll;height:auto;overflow-y: scroll;',
+                   shinycssloaders::withSpinner(
+                     plotly::plotlyOutput(ns("univariatePlot")))
+                 ))
+          ),
+          downloadButton(ns('downloadPlot'), "Download Plot")
       )
     )
   )
@@ -39,6 +42,7 @@ univariatePlotPanelUI <- function(id) {
 
 univariatePlotPanel <- function(input, output, session, getData) {
   ns <- session$ns
+  rv <- reactiveValues()
 
   observe({
     marker_choices <- getData()[[1]][,1]
@@ -46,20 +50,23 @@ univariatePlotPanel <- function(input, output, session, getData) {
                       choices = marker_choices)
   })
 
-  output$colorChooser <- renderUI({
-    features <- sort(unique(unlist(getData()[[2]][,input$sample_group])))
-    lapply(1:length(features), function(x){
-      list(colourInput(paste("univariatePanel-col",x, sep="_"), features[x], "purple"))
-    })
-  })
-
+  # output$colorChooser <- renderUI({
+  #   features <- sort(unique(unlist(getData()[[2]][,get(input$sample_group)])))
+  #   LL <- vector("list", length(features))
+  #   for(i in 1:length(features)) {
+  #     LL[[i]] <- list(colourInput(paste("univariatePanel-col",i, sep="_"), features[i], "purple"))
+  #   }
+  #   return(LL)
+  # })
+  #
+  #
   colors <- reactive({
-    features <- sort(unique(unlist(getData()[[2]][, input$sample_group ])))
+    features <- sort(unique(unlist(getData()[[2]][, get(input$sample_group)])))
     LL <- lapply(seq_along(features), function(i) {
       input[[paste("col", i, sep="_")]]
     })
     if (is.null(input$col_1) ) {
-      features <- sort(unique(unlist(getData()[[2]][,input$sample_group])))
+      features <- sort(unique(unlist(getData()[[2]][,get(input$sample_group)])))
       fill_colors <- rep("purple", length(features))
     } else {
       fill_colors <- unlist(LL)
@@ -67,36 +74,38 @@ univariatePlotPanel <- function(input, output, session, getData) {
     return(fill_colors)
   })
 
+  toListen <- reactive({
+    list(input$marker_selector, input$sample_group, input$x_axis_text_angle)
+  })
 
-  map_marker_category <- reactive({
-    marker_stats <- data.frame(getData()[[1]])
-    marker_stats <- subset(marker_stats, marker_stats[,1]==input$marker_selector)
-    marker_stats <- data.frame(names(marker_stats[-1]), t(marker_stats[-1]))
+  observeEvent(toListen(),{
+    abundance <- getData()[[1]]
+    marker_long <- melt(abundance[UniProtID == input$marker_selector],
+                        id.vars = 'UniProtID')
+    setnames(marker_long, c('variable', 'value'), c('Sample', 'Exprs'))
+    marker_stats <- marker_long[getData()[[2]], on='Sample']
 
-    names(marker_stats) <- c("Sample","Exprs")
-    sample_mapping_df <- data.frame(getData()[[2]][c("Sample", input$sample_group)])
-    #sample_mapping_df$Sample <- gsub("-", ".", sample_mapping_df$Sample)
-    #sample_mapping_df[,input$sample_group] <- gsub("-", ".", sample_mapping_df[,input$sample_group])
-    return(merge(marker_stats, sample_mapping_df, by="Sample"))
+    rv$plot <- ggplot(data = marker_stats,
+                      aes(x = get(input$sample_group), y = Exprs ,
+                          fill = get(input$sample_group)))+
+      geom_boxplot(outlier.color = 'red')+
+      labs(x = input$sample_group, fill = input$sample_group)+
+      theme_bw()+
+      theme(text = element_text(size = 16),
+            axis.text.x = element_text(angle = input$x_axis_text_angle),
+            legend.position = 'top')
   })
 
   output$univariatePlot <- renderPlotly({
-    req(input$sample_group)
-    req(input$marker)
-    ggplotly(
-      ggplot(data = map_marker_category(),
-             aes(x = get(input$sample_group), y=  Exprs , fill = get(input$sample_group)))+
-        scale_fill_manual(values = colors())+
-        geom_boxplot(outlier.color = 'red')+
-        labs(x = input$sample_group, fill = input$sample_group)+
-        theme_bw()+
-        theme(text = element_text(size = 16),
-              axis.text.x = element_text(angle = input$x_axis_text_angle),
-              legend.position = 'top'),
-
-      width = session$clientData$output_pid_width,
-      height = session$clientData$output_pid_width
-    )
+    req(!is.null(rv$plot))
+    #browser()
+    if(length(unique(colors())) == 1){
+      ggplotly(rv$plot)
+    } else {
+      rv$plot <- rv$plot+
+        scale_fill_manual(values = )
+      ggplotly(rv$plot)
+    }
   })
 
 
@@ -116,8 +125,7 @@ univariatePlotPanel <- function(input, output, session, getData) {
                 pdf(file, width = input$plot_width, height = input$plot_height)
               }
       )
-      plot = gen_univariateplot()
-      print(plot)
+      print(rv$plot)
       dev.off()
     }
   )
