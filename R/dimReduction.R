d66475bf517635756b9547bb54584d308259d2df
Sumedh R. Sankhe <sumedh.sankhe@gmail.com>
2022-08-05 14:52:06 -0700

    dim redux tsne update

--- R/dimReduction.R
+++ R/dimReduction.R
@@ -3,7 +3,7 @@ dimReductionUI <- function(id) {
   tagList(
     h3("Dimensionality Reduction"),
     fluidRow(
-      box( title = "Dimensionality Reduction & Visualization", width = 4, status = "warning",
+      box( title = "Control Box", width = 4, status = "primary",
            tabBox(id = "dimred", height = "100%", width = "100%",
                   tabPanel("Projection",
                            uiOutput(ns("dimred_markers_selector")),
@@ -12,9 +12,11 @@ dimReductionUI <- function(id) {
                                        label = 'Projection Method: ',
                                        choices = c('PCA', 'tSNE', 'UMAP')
                            ),
-                           numericInput(inputId = ns('x_dim'), label = 'X-axis Dimension: ', value='1'),
-                           numericInput(inputId = ns('y_dim'), label = 'Y-axis Dimension: ', value='2'),
-                           
+                           numericInput(inputId = ns('x_dim'),
+                                        label = 'X-axis Dimension: ', value='1'),
+                           numericInput(inputId = ns('y_dim'),
+                                        label = 'Y-axis Dimension: ', value='2'),
+
                            actionButton(ns("dimredButton"),
                                         label = "Submit",
                                         icon("paper-plane"),
@@ -31,8 +33,10 @@ dimReductionUI <- function(id) {
                     conditionalPanel("input.drmethod == 'UMAP'", ns=ns,
                       umapPanelUI(ns("umap"))
                     ),
-                    numericInput(inputId = ns('plot_width'), label = 'Plot Width: ', value='15'),
-                    numericInput(inputId = ns('plot_height'), label = 'Plot Height: ', value='10'),
+                    numericInput(inputId = ns('plot_width'),
+                                 label = 'Plot Width: ', value='15'),
+                    numericInput(inputId = ns('plot_height'),
+                                 label = 'Plot Height: ', value='10'),
                     selectInput(inputId = ns('plot_type'),
                                 label = 'Plot Type: ',
                                 choices = c('svg', 'png', 'pdf')
@@ -42,41 +46,34 @@ dimReductionUI <- function(id) {
       ),
       box(
         status = "warning", width = 8,
-          tabBox(id = "displayTab", height = "100%", width = "100%",
-            tabPanel(title="Projections",
-              shinycssloaders::withSpinner(
-                plotlyOutput(ns("projectedPlot")
-                  # , 
-                  # dblclick = ns("plot_dblclick"), 
-                  # brush = brushOpts(id = ns("plot_brush"), resetOnNew = TRUE) 
-                )
-              ),
-              HTML("<br>"),
-              downloadButton(ns('downloadProjPlot'), "Download Plot")
-            ),
+        tabBox(id = "displayTab", height = "100%", width = "100%",
+               tabPanel(title="Projections",
+                        (div(style='width:auto;overflow-x: scroll;height:auto;overflow-y: scroll;',
+                        shinycssloaders::withSpinner(
+                          plotlyOutput(ns("projectedPlot")))
+                          )
+                        ),
+                        HTML("<br>"),
+                        downloadButton(ns('downloadProjPlot'), "Download Plot")
+               ),
 
-            tabPanel(title="Contributions",
-               shinycssloaders::withSpinner(
-                 plotOutput(ns("eigen_contrib_plot")
-                            # , 
-                            # dblclick = ns("plot_dblclick"), 
-                            # brush = brushOpts(id = ns("plot_brush"), resetOnNew = TRUE) 
-                 )
-               ),
-               br(),
-               shinycssloaders::withSpinner(
-                 plotOutput(ns("var_contrib_plot")
-                            # , 
-                            # dblclick = ns("plot_dblclick"), 
-                            # brush = brushOpts(id = ns("plot_brush"), resetOnNew = TRUE) 
-                 )
-               ),
-               br(),
-               dataTableOutput(ns("pca_contrib_results")),
-               HTML("<br>"),
-               downloadButton(ns("download_pca_contrib"), "Download csv")
-            )
-          )
+               tabPanel(title="Contributions",
+                        (div(style='width:auto;overflow-x: scroll;height:auto;overflow-y: scroll;',
+                        shinycssloaders::withSpinner(
+                          plotOutput(ns("eigen_contrib_plot")))
+                          )
+                        ),
+                        br(),
+                        shinycssloaders::withSpinner(
+                          plotOutput(ns("var_contrib_plot")
+                          )
+                        ),
+                        br(),
+                        dataTableOutput(ns("pca_contrib_results")),
+                        HTML("<br>"),
+                        downloadButton(ns("download_pca_contrib"), "Download csv")
+               )
+        )
       )
     )
   )
@@ -84,87 +81,57 @@ dimReductionUI <- function(id) {
 
 dimReduction <- function(input, output, session, getData) {
   ns <- session$ns
-  ranges <- reactiveValues(x = NULL, y = NULL)
+  #ranges <- reactiveValues(x = NULL, y = NULL)
+  rv <- reactiveValues()
 
-  getClassificationData <- reactive({
-    
-    phenoTable <- data.frame(getData()[[2]])
-    phenoTable$Sample <- gsub("-", ".", phenoTable$Sample)
-    phenoTable$Condition <- gsub("-", ".", phenoTable$Condition)
-    phenoTable
-  })
-  
-  getMatrixData <- reactive({
-    df <- data.frame(getData()[[1]])
-    row.names(df) <- unlist(df[1])
-    df <- t(df[-1])
-    as.data.frame(df)
-  })
+  observe({
+    abundance <- as.matrix(getData()[[1]], rownames = T)
+    rv$abundance <- as.data.frame(t(abundance))
+    rv$annotation <- getData()[[2]]
+    rv$annotation[,':='(Sample = gsub('-','.', Sample),
+                       Condition = gsub('-','.', Condition))]
 
-  ########################
-  ##select numeric columns
-  ########################
-  getNumericCols <- reactive({
-      data <- getMatrixData()
-      cols <- colnames(data)
-      classes <- sapply(data, class)
-      univ <- names(classes[classes == "numeric"])
-      cols <- cols[cols %in% univ]
-      cols
+    isNumeric <- vapply(rv$abundance, is.numeric, logical(1))
+    rv$numeric.cols <- names(isNumeric)[isNumeric]
+    rv$feature_choices <- names(rv$abundance)
   })
 
   output$selectNumericCols <- renderUI({
-      cols <- getNumericCols()
-      selectInput(ns("numericCol"), "Columns with Numeric Values",
-                  choices = cols,
-                  multiple=TRUE,
-                  selected = NULL)
-  })  
-  
-  get_feature_choices <- reactive({
-    return( getMatrixData() %>% names() )
+    selectInput(ns("numericCol"), "Columns with Numeric Values",
+                choices = rv$numeric.cols, multiple=TRUE, selected = NULL)
   })
-  
+
   output$dimred_markers_selector <- renderUI({
-    selectizeInput(inputId = ns('smarkers'),
-                  label = 'Select Features: ', 
-                  multiple=TRUE,
-                  choices = getNumericCols(),
-                  options = list(maxItems = 100, placeholder = 'All')
+    selectizeInput(inputId = ns('smarkers'), label = 'Select Features: ',
+                   multiple=TRUE, choices = rv$numeric.cols,
+                   options = list(maxItems = 100, placeholder = 'All')
     )
   })
-  
+
   output$cmarker_selector <- renderUI({
-    selectInput(inputId = ns('cmarker'),
-               label = 'Color By Feature: ',
-               choices = get_classification_choices() #get_feature_choices()
-    )  
+    selectInput(inputId = ns('cmarker'), label = 'Color By Feature: ',
+                choices = get_classification_choices()
+    )
   })
-    
-  selected_markers <- reactive({
-    return(input$smarkers)
-  })
-  
+
+  tsne_projs <- callModule(tsnePanel, "tsne", rv$abundance, input$smarkers,
+                           rv$annotation, proj_dims)
+  pca_projs  <- callModule(pcaPanel, "pca", rv$abundance, input$smarkers,
+                           getClassificationData)
+  umap_projs <- callModule(umapPanel, "umap", rv$abundance, input$smarkers,
+                           rv$annotation, proj_dims)
+
+
   proj_dims <- reactive({
     return(c(input$x_dim, input$y_dim))
   })
-  
+
   get_classification_choices <- reactive({
     return( names(getData()[[2]])[-1] )
   })
-  
-  # output$classificationChoice <- renderUI({
-  #   selectInput(inputId = ns('classificationmethod'),
-  #               label = 'Classification: ',
-  #               choices = get_classification_choices()
-  #   )
-  # })
-    
-  tsne_projs <- callModule(tsnePanel, "tsne", getMatrixData, selected_markers, getClassificationData, proj_dims)
-  pca_projs  <- callModule(pcaPanel, "pca", getMatrixData, selected_markers, getClassificationData)
-  umap_projs <- callModule(umapPanel, "umap", getMatrixData, selected_markers, getClassificationData, proj_dims)
-  
-  compute_projs <- eventReactive( input$dimredButton, {
+
+
+  observeEvent( input$dimredButton, {
     set.seed(100)
     # # Create a Progress object
     # progress <- shiny::Progress$new()
@@ -182,33 +149,38 @@ dimReduction <- function(input, output, session, getData) {
               proj_results <- umap_projs()
             }
     )
-    return( proj_results )
+
+
+    plotdt <-
+
+    rv$plot <- ggplot(data = )
+
   })
-  
+
   get_projectedplot <- reactive({
     req(compute_projs())
     #req(selected_markers())
-    
+
     if (isolate(input$drmethod) == 'PCA') {
       color_marker <- compute_projs()$projInput %>% dplyr::select( isolate(input$cmarker) ) %>% unlist()
       projected_ouput <- compute_projs()$projOutput
       projected_ouput$tooltip <- rownames(projected_ouput)
       p <- fviz_pca_ind(projected_ouput,
                                 axes = c(isolate(input$x_dim), isolate(input$y_dim)),
-                                addEllipses = TRUE, 
-                                geom.ind = "point", 
-                                geom.text=color_marker, 
-                                pointshape=21, 
-                                pointsize = 2.5, 
-                                fill.ind = color_marker, 
-                                repel=TRUE) + scale_color_viridis_d() 
+                                addEllipses = TRUE,
+                                geom.ind = "point",
+                                geom.text=color_marker,
+                                pointshape=21,
+                                pointsize = 2.5,
+                                fill.ind = color_marker,
+                                repel=TRUE) + scale_color_viridis_d()
     }
     else {
       plot_data <- cbind(compute_projs()$projOutput, compute_projs()$projInput %>% dplyr::select( isolate(input$cmarker) ))
-      
+
       if (class(plot_data[[ isolate(input$cmarker) ]]) %in% c("numeric","integer") ) {
         p <- ggplot(plot_data, aes_string(x = names(plot_data)[1], y = names(plot_data)[2]), group= isolate(input$cmarker) ) +
-            geom_point(aes_string(color=input$cmarker), size=3) + scale_color_viridis_c() 
+            geom_point(aes_string(color=input$cmarker), size=3) + scale_color_viridis_c()
       } else {
         p <- ggplot(plot_data, aes_string(x = names(plot_data)[1], y = names(plot_data)[2]), group= isolate(input$cmarker) ) +
             geom_point(aes_string(color= isolate(input$cmarker) ), size=3) + scale_colour_viridis_d()
@@ -217,7 +189,7 @@ dimReduction <- function(input, output, session, getData) {
     }
     p
   })
-  
+
   output$projectedPlot <- renderPlotly({
     p <- ggplotly(get_projectedplot(), tooltip = "text")
     for (i in 1:length(p$x$data)){
@@ -227,11 +199,11 @@ dimReduction <- function(input, output, session, getData) {
     }
     p
   })
-  
+
   output$eigen_contrib_plot <- renderPlot({
     req(compute_projs())
     #req(selected_markers())
-    
+
     if (isolate(input$drmethod) == 'PCA') {
       p <- fviz_eig(compute_projs()$projOutput, addlabels = TRUE, ylim = c(0, 50))
     }
@@ -239,27 +211,27 @@ dimReduction <- function(input, output, session, getData) {
       p <- ggplot()
     }
     p
-    
+
   })
 
   output$var_contrib_plot <- renderPlot({
     req(compute_projs())
     #req(selected_markers())
-    
+
     if (isolate(input$drmethod) == 'PCA') {
-      p <- fviz_pca_var(compute_projs()$projOutput, 
+      p <- fviz_pca_var(compute_projs()$projOutput,
                         axes = c(isolate(input$x_dim), isolate(input$y_dim)),
-                        col.var = "contrib", 
-                        select.var=list(contrib=5), 
+                        col.var = "contrib",
+                        select.var=list(contrib=5),
                         repel=TRUE)
     }
     else {
       p <- ggplot()
     }
     p
-    
+
   })
-  
+
   output$pca_contrib_results <- DT::renderDataTable({
     req(compute_projs())
     if (isolate(input$drmethod) == 'PCA') {
@@ -269,7 +241,7 @@ dimReduction <- function(input, output, session, getData) {
       data.frame()
     }
   })
-  
+
   output$download_pca_contrib <- downloadHandler(
     filename = function() {
       "PCA_Contributions.csv"
@@ -281,7 +253,7 @@ dimReduction <- function(input, output, session, getData) {
       }
     }
   )
-  
+
   output$downloadProjPlot <- downloadHandler(
     filename = function() {
       paste("projected_plot", input$plot_type, sep=".")
@@ -303,7 +275,7 @@ dimReduction <- function(input, output, session, getData) {
       dev.off()
     }
   )
-  
+
   # output$downloadProjPlot <- downloadHandler(
   #   filename = "projected_plot.svg",
   #   contentType = "image/svg",
@@ -314,7 +286,7 @@ dimReduction <- function(input, output, session, getData) {
   #     dev.off()
   #   }
   # )
-  
+
   # # When a double-click happens, check if there's a brush on the plot.
   # # If so, zoom to the brush bounds; if not, reset the zoom.
   # observeEvent(input$plot_dblclick, {
@@ -322,7 +294,7 @@ dimReduction <- function(input, output, session, getData) {
   #   if (!is.null(brush)) {
   #     ranges$x <- c(brush$xmin, brush$xmax)
   #     ranges$y <- c(brush$ymin, brush$ymax)
-  # 
+  #
   #   } else {
   #     ranges$x <- NULL
   #     ranges$y <- NULL
